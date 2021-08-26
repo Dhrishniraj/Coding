aabfdeed678ca912716c8a63388726eb98cdb916
Dhrishniraj <83451121+Dhrishniraj@users.noreply.github.com>
2021-08-26 17:07:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 var students = [];
 function submit(){
-    var name1 = document.getElementById("student1").value;
-    var name2 = document.getElementById("student2").value;
-    var name3 = document.getElementById("student3").value;
-    var name4 = document.getElementById("student4").value;
-    students.push(name1);
-    students.push(name2);
-    students.push(name3);
-    students.push(name4);
-    console.log(students);
-    document.getElementById("students_names").innerHTML=students;
-    document.getElementById("submit").style.display="none";
-    document.getElementById("sorting").style.display="inline-block";
+  var studiv = [];
+  for(var j = 1; j <= 4; j++){
+    var studentnew = document.getElementById("name_of_the_student_" + j).value;
+    console.log(studentnew);
+    students.push(studentnew);
+  }
+  console.log(students);
+  console.log(students.length);
+  for(var k = 0; k < students.length; k++){
+    studiv.push("NAME - " + students[k] + "<br>");
+    console.log(studiv);
+  }
+  document.getElementById("display_name_with_commas").innerHTML=studiv;
+  var remcom = studiv.join(" ");
+  document.getElementById("display_name_without_commas").innerHTML=remcom;
+  document.getElementById("submit_button").style.display="none";
+  document.getElementById("sort_button").style.display="inline-block";
 }
 function sorting(){
-    students.sort();
-    console.log(students);
-    document.getElementById("students_names").innerHTML=students;
+  students.sort();
+  console.log(students);
+  var sorted = [];
+  console.log(students.length);
+  for(var i = 0; i < students.length; i++){
+    sorted.push("NAME - " + students[i] + "<br>");
+    console.log(sorted);
+  }
+  remcom = sorted.join(" ");
+  console.log(remcom);
+  document.getElementById("display_name_without_commas").innerHTML=remcom;
 }
