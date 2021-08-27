1714c3d3f98f18fec95156ab2364bdd9d8deb402
Dhrishniraj <83451121+Dhrishniraj@users.noreply.github.com>
2021-08-27 12:05:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,49 @@
-var students = [];
-function submit(){
-  var studiv = [];
-  for(var j = 1; j <= 4; j++){
-    var studentnew = document.getElementById("name_of_the_student_" + j).value;
-    console.log(studentnew);
-    students.push(studentnew);
-  }
-  console.log(students);
-  console.log(students.length);
-  for(var k = 0; k < students.length; k++){
-    studiv.push("NAME - " + students[k] + "<br>");
-    console.log(studiv);
-  }
-  document.getElementById("display_name_with_commas").innerHTML=studiv;
-  var remcom = studiv.join(" ");
-  document.getElementById("display_name_without_commas").innerHTML=remcom;
-  document.getElementById("submit_button").style.display="none";
-  document.getElementById("sort_button").style.display="inline-block";
-}
-function sorting(){
-  students.sort();
-  console.log(students);
-  var sorted = [];
-  console.log(students.length);
-  for(var i = 0; i < students.length; i++){
-    sorted.push("NAME - " + students[i] + "<br>");
-    console.log(sorted);
-  }
-  remcom = sorted.join(" ");
-  console.log(remcom);
-  document.getElementById("display_name_without_commas").innerHTML=remcom;
+var names_of_people = [];
+    
+function submit()
+{
+    var GuestName = document.getElementById("name1").value;
+	// use the push function as - names_of_people.push(GuestName);
+	console.log(GuestName);    
+	names_of_people.push(GuestName);
+    console.log(names_of_people);
+    var lenght_of_name = names_of_people.length;
+    console.log(lenght_of_name);
+	document.getElementById("display_name").innerHTML=names_of_people;
+   }
+
+
+
+function show()
+{
+	var i= names_of_people.join("<br>");
+	console.log(names_of_people);
+	document.getElementById("p1").innerHTML=i.toString();
+	document.getElementById("sort_button").style.display="block";
+	// display the name in the id= sort_button
+	}
+
+
+function sorting()
+	{
+		names_of_people.sort()
+		var i= names_of_people.join("<br>");
+		console.log(names_of_people);		
+		document.getElementById("sorted").innerHTML=i.toString();
+		}
+
+
+function searching()
+{
+	var s= document.getElementById("s1").value;
+	var found=0;
+	var j;
+	for(j=0; j<names_of_people.length; j++)
+		{
+			if(s==names_of_people[j]){
+				found=found+1;
+			}	
+		}
+	document.getElementById("p2").innerHTML="name found "+found+" time/s";
+	console.log("found name "+found+" time/s");
 }
